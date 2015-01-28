21d4a21a3f2214f4e33cf3b70b22458c459c79ec
Noah Portes Chaikin <noah@porteschaikin.com>
2015-01-27 21:11:30 -0500

    Node.js example.

---
+++ examples/node/validator.js
@@ -0,0 +1,25 @@
+// A portable validator for a very
+// basic user object.
+
+var Credible = require('credible');
+
+var validator = new Credible();
+
+credible
+  // firstName, lastName, email,
+  // and password are required.
+  .rule(['firstName', 'lastName', 'email', 'password'], 'presence')
+
+  // email must be a valid e-mail address.
+  // Only checks if e-mail is present.
+  .rule('email', 'email', { if: function (obj) { return obj.email; } } )
+
+  // password must match passwordConfirmation.
+  // Only checks if password is present.
+  .rule('password', 'operator', { equalTo: 'passwordConfirmation', if: function (obj) { return obj.password } })
+
+module.exports = function (object) {
+  // Run the validator on the object
+  // passed above. Returns a promise.
+  return credible.run(object);
+}
